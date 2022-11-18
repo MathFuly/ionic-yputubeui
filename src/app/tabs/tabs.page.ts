22f105baea521f250e3c9be0d09ecfc6e489218e
MatFuly <matheus.fuly.ferreira1@gmail.com>
2022-11-18 13:31:04 -0300

    first commit

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { SheetPage } from '../sheet/sheet.page';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+  constructor(private modalCtrl: ModalController) {}
 
-  constructor() {}
+  async add() {
+    const modal = await this.modalCtrl.create({
+      component: SheetPage,
+      breakpoints: [0.5, 0.6],
+      initialBreakpoint: 0.5,
+      handle: false,
+    });
 
+    await modal.present();
+  }
 }
